922a903938b185b46b5b91b7b3300f8470d4034e
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2023-10-21 22:38:11 +0400

    GeneralHeaderInclude

---
+++ HelloWorld/website1/inc/header.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>My Website</title>
+    </head>
+    <body>
+        <ul>
+            <li><a href="/HelloWorld/website1/index.php">Home</a></li>
+            <li><a href="/HelloWorld/website1/about.php">About Us</a></li>
+            <li><a href="/HelloWorld/website1/contact.php">Contact Us</a></li>
+        </ul>
