2d2d5ce5e6f280eec9127950bd0e80612749c567
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2023-10-21 22:38:44 +0400

    Website1Index

---
+++ HelloWorld/website1/index.php
@@ -0,0 +1,7 @@
+<?php
+    // 'require' instead of 'include' makes it wont print anything else without that content
+    include "/src/PHPProgram/HelloWorld/website1/inc/header.php"
+?>
+    <h1>Home</h1>
+    <h2>Header 2</h2>
+<?php include "/src/PHPProgram/HelloWorld/website1/inc/footer.php"?>
