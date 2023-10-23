d1bb621cdd35e74ef688d0dcbd39ff47bd1f3193
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2023-10-23 14:05:16 +0400

    LowPowerCommit

--- TravelForm/action.php
+++ TravelForm/action.php
@@ -3,6 +3,6 @@
     echo "<br>Your Contact is : ".htmlspecialchars($_POST['contact']);
     $mysqli = mysqli_connect('localhost', 'root', '', 'form', 3307);
     $mysqli->execute_query("
-        INSERT INTO `user`(name,contact) values(".htmlspecialchars($_POST['name']).",".htmlspecialchars($_POST['contact'])
+        INSERT INTO `user`(`name`,`contact`) values('".htmlspecialchars($_POST['name'])."','".htmlspecialchars($_POST['contact'])."'"
     );
 ?>
