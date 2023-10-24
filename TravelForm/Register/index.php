50c14786db1a4e2c46d750e21cfd0f4c5d0cd990
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2023-10-24 10:42:14 +0400

    RegisterForm

---
+++ TravelForm/Register/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Register | Travel Note</title>
+    </head>
+    <body>
+        <h1>Registration Form</h1>
+        <h3>Enter Details:</h3>
+        <form action="action.php" method="post">
+            <label for='email'>Your Email:</label>
+            <input name='email' id='email' type="email">
+            <br>
+            <label for='password'>Password:</label>
+            <input name='password' id='password' type='password'>
+            <br>
+            <button type="submit">Submit</button>
+        </form>
+    </body>
+</html>
