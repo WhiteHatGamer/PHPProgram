4c1fe079272bcc6feb10c8b3da1378f1761dbd90
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2023-10-24 14:04:07 +0400

    ReadNoteListingFromDb

--- TravelForm/Dashboard/ReadNote/index.php
+++ TravelForm/Dashboard/ReadNote/index.php
@@ -1,3 +1,10 @@
+<?php
+
+    // Adding MySQL Connection
+    require "../../var.php";
+
+    // Starting Session
+    session_start();
 
     // Checking If User is Logged In
     if(!isset($_SESSION['email'])){
@@ -5,6 +12,18 @@
         echo '<a href="../index.php">Home</a><br>';
         exit;
     }
+    // Getting Every Notes Saved from User
+    $result = $Mysqli->query("SELECT * FROM $NoteTable WHERE email='".$_SESSION['email']."'");
+
+    // Looping Through Result
+    echo "<br><h5>Saved Notes:</h5><br>";
+    foreach ($result->fetch_all() as $key) {
+        echo "Modified Time:".$key[0];
+        echo "&emsp;";
+        echo "Note: ".$key[2];
+        echo "<br>";
+    }
+
     // Return To Dashboard
     echo "<br>Goto ";
     echo "<a href='../index.php'>Dashboard</a>";
