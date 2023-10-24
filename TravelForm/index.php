07175e424d7370fd313dd6103b8379f93fbb1f80
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2023-10-24 14:12:42 +0400

    Typos

--- TravelForm/index.php
+++ TravelForm/index.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="icon" href="Assets/favicon.ico" type="image/x-icon"/>
-        <title>Home | Travel Note</title>
+        <title>Home | Travel Notes</title>
     </head>
     <body>
         <?php include "inc/header.php";?>
