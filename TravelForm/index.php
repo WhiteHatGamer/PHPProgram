24fe6edfb65a55b1c10b74e8f9c6781e65e3dd15
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2023-10-24 13:47:53 +0400

    TriedHeader

--- TravelForm/index.php
+++ TravelForm/index.php
@@ -3,13 +3,15 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="icon" href="Assets/favicon.ico" type="image/x-icon"/>
         <title>Home | Travel Note</title>
     </head>
     <body>
-        <h1>Home</h1>
-        <br>
+        <?php include "inc/header.php";?>
+        <h3>Home</h3>
         <a href="Register/index.php">Register</a>
         <br>
         <a href="Login/index.php">LogIn</a>
+        <?php include "inc/footer.php";?>
     </body>
 </html>
