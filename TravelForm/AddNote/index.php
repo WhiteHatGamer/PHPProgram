d0e2ef45ea7c4928bc2ac2726f8e96ac22e0e5ed
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2023-10-24 10:47:23 +0400

    CreateNoteForm

---
+++ TravelForm/AddNote/index.php
@@ -0,0 +1,22 @@
+<?php
+    // Starting Session
+    session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Add Note | Travel Note</title>
+    </head>
+    <body>
+        <h1>Add Note</h1>
+        <h3>Enter Details:</h3>
+        <form action="action.php" method="post">
+            <label for='note'>Your Note:&emsp;&nbsp;</label>
+            <input name='note' id='note' type="text">
+            <br>
+            <button type="submit">Submit</button>
+        </form>
+    </body>
+</html>
