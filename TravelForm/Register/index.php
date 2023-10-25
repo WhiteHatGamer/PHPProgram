f68dc32ca2810b52cb6c50c965a9b20a0a40fc31
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2023-10-25 19:21:44 +0400

    AddedMoreInputs

--- TravelForm/Register/index.php
+++ TravelForm/Register/index.php
@@ -9,11 +9,20 @@
         <h1>Registration Form</h1>
         <h3>Enter Details:</h3>
         <form action="action.php" method="post">
-            <label for='email'>Your Email:</label>
-            <input name='email' id='email' type="email" autofocus required>
-            <br>
-            <label for='password'>Password:</label>
-            <input name='password' id='password' type='password' required>
+            <label for="image">Picture: </label>
+            <input name="image" type="file"><br>
+            <label for="name">Name: </label>
+            <input name="name" type="text" autofocus><br>
+            <label for="gender">Gender: </label>
+            <input name="gender" type="radio">Male<input name="gender" type="radio">Female<br>
+            <label for="contact">Number: </label>
+            <input name="contact" type="tel"><br>
+            <label for="dob">Date of Birth: </label>
+            <input name="dob" type="date"><br>
+            <label for='email'>Email*: </label>
+            <input name='email' id='email' type="email" required><br>
+            <label for='password'>Password*: </label>
+            <input name='password' id='password' type='password' required><br>
             <br>
             <button type="submit">Submit</button>
         </form>
