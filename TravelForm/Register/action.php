be86a11cf669e6334b41695400765b77f95ac1ce
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2023-11-01 10:42:18 +0400

    PasswordSecurity

--- TravelForm/Register/action.php
+++ TravelForm/Register/action.php
@@ -9,7 +9,7 @@
     }
 
     // Checking if User Already Exists
-    $result = $Mysqli->query("SELECT * FROM user WHERE email LIKE '".htmlspecialchars($_POST['email'])."'");
+    $result = $Mysqli->query("SELECT * FROM user WHERE email='".htmlspecialchars($_POST['email'])."'");
     echo "<br>";
     if($result->num_rows>0){
 
@@ -21,7 +21,7 @@
         
         // User Don't Exist. Adding User
         $result = $Mysqli->query(
-            "INSERT INTO $UserTable(`email`, `password`) values('".htmlspecialchars($_POST['email'])."','".htmlspecialchars($_POST['password'])."')"
+            "INSERT INTO $UserTable(`email`, `password`) values('".htmlspecialchars($_POST['email'])."',md5('".htmlspecialchars($_POST['password'])."'))"
         );
         echo "<h3>User Added</h3>";
         echo "Go to Login Page for Adding Your First Note";
