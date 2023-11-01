6dff7a5b21bc5b37bf1b6285832e16b0b9650c52
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2023-11-01 11:20:09 +0400

    Using__DIR__

--- HelloWorld/website1/index.php
+++ HelloWorld/website1/index.php
@@ -1,6 +1,11 @@
 <?php
+    // Starting Session
+    session_start();
+
+    echo "<h3>Header</h3>";
     // 'require' instead of 'include' makes it wont print anything else without that content
-    include "/src/PHPProgram/HelloWorld/website1/inc/header.php"
+    include __DIR__."/inc/header.php";
+    require_once __DIR__."/../vars.php";
 ?>
     <h1>Home</h1>
     <h2>Header 2</h2>
