4499b12cf8facf4ed820b6497f5cd3fef4069e4d
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2023-11-01 10:39:25 +0400

    addedHelloWorldHyperLink

--- HelloWorld/website1/inc/header.php
+++ HelloWorld/website1/inc/header.php
@@ -4,6 +4,7 @@
     </head>
     <body>
         <ul>
+            <li><a href="/HelloWorld/index.php">Hello World</a></li>
             <li><a href="/HelloWorld/website1/index.php">Home</a></li>
             <li><a href="/HelloWorld/website1/about.php">About Us</a></li>
             <li><a href="/HelloWorld/website1/contact.php">Contact Us</a></li>
