699aa322cdf12ef7b31abe3a9efca9a183491575
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2023-11-01 11:21:31 +0400

    SettingCookies

--- HelloWorld/website1/index.php
+++ HelloWorld/website1/index.php
@@ -6,6 +6,12 @@
     // 'require' instead of 'include' makes it wont print anything else without that content
     include __DIR__."/inc/header.php";
     require_once __DIR__."/../vars.php";
+
+    // Setting Cookies
+    $CookieName = $_SERVER['REMOTE_ADDR'];
+    $CookieValue = '';
+
+    setcookie($CookieName, $CookieValue, time()+(86400*30),'/');
 ?>
     <h1>Home</h1>
     <h2>Header 2</h2>
