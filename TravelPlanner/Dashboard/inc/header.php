a3e27eb5d2e77ff28c62505868e8330c1c6230d2
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2023-11-06 13:17:53 +0400

    HeaderWithHomeAndDashBoard

---
+++ TravelPlanner/Dashboard/inc/header.php
@@ -0,0 +1,4 @@
+<header>
+    <div style="width: 96px; padding: .5px; height: 20px; border: 2px solid blue;"><a href="/TravelPlanner/index.php">Travel Planner</a></div>
+    <a style="position:absolute; top: 12px ;right:65%;" href="/TravelPlanner/Dashboard/index.php"><button>Go to Dashboard</button></a>
+</header>
