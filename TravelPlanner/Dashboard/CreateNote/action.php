ac06ef935cbe162f6feb44c8fa6061328826e42a
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2023-11-06 13:18:17 +0400

    IfLogOutToHome

--- TravelPlanner/Dashboard/CreateNote/action.php
+++ TravelPlanner/Dashboard/CreateNote/action.php
@@ -6,7 +6,7 @@
     // Checking If User is Logged In
     if(!isset($_SESSION['email'])){
         echo "<h3>You Are Logged out Please Login Again!!!</h3><br>";
-        echo '<a href="../index.php">Home</a><br>';
+        echo '<a href="../../index.php">Home</a><br>';
         exit;
     }
     ?>
