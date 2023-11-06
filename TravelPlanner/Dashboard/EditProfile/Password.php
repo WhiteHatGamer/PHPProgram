65262969eda7b91e7e76e03bc3c06d706bf7cf1a
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2023-11-06 11:45:28 +0400

    PasswordForm

--- TravelPlanner/Dashboard/EditProfile/Password.php
+++ TravelPlanner/Dashboard/EditProfile/Password.php
@@ -35,10 +35,21 @@
         }
     }
 ?>
-<form action="<?php echo $_SERVER['PHP_SELF']?>" method="post">
-    <label for="OldPassword">Old Password: </label>
-    <input type="password" name="OldPassword"><br>
-    <label for="NewPassword">New Password: </label>
-    <input type="password" name="NewPassword"><br>
-    <button name="change" type="submit">Change Password</button>
-</form>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Change Password</title>
+</head>
+<body>
+    <h3>Change Password</h3>
+    <form action="<?php echo $_SERVER['PHP_SELF']?>" method="post">
+        <label for="OldPassword">Old Password: </label>
+        <input type="password" name="OldPassword"><br>
+        <label for="NewPassword">New Password: </label>
+        <input type="password" name="NewPassword"><br>
+        <button name="change" type="submit">Change Password</button>
+    </form>
+</body>
+</html>
