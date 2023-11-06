421ba69bfaabc63048109216ab9d7b54d9f2961b
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2023-11-06 11:56:01 +0400

    CommentsAndClientSideVerification

--- TravelPlanner/Dashboard/EditProfile/Password.php
+++ TravelPlanner/Dashboard/EditProfile/Password.php
@@ -11,16 +11,19 @@
     // Checking If User is Logged In
     if(!isset($_SESSION['email'])){
         echo "<h3>You Are Logged out Please Login Again!!!</h3><br>";
-        echo '<a href="../index.php">Home</a><br>';
+        echo '<a href="../../index.php">Home</a><br>';
         exit;
     }
 
+    // Checking If Submitted
     if(isset($_POST['change'])){
         $changeFlag = false;
 
         try {
+            // Checking if User's Old Password is Same
             $result = $Mysqli->query("SELECT EXISTS(SELECT * FROM user WHERE email='{$_SESSION['email']}' && password=md5('{$_POST['OldPassword']}'))");
             if($result->fetch_row()[0]){
+                // if Same Update Password
                 $Mysqli->query(
                     "UPDATE $UserTable
                     SET password=md5('{$_POST['NewPassword']}')
@@ -31,12 +34,18 @@
                 echo "<br>You Old Password is Incorrect Please Try Again!!!<br>";
             }
         } catch (Exception $e) {
+
+            // Catches Exception
             echo "<h3>ERROR Try Again! : {$e->getMessage()}</h3><br>";
         }
         
         if($changeFlag){
+            // Logging Out After Changing Password
             echo "<h3>Your Password is Changed. Please Login Again!</h3><br>";
-            echo '<a href="../index.php">Home</a><br>';
+            echo '<a href="../../index.php">Home</a><br>';
+            
+            // Clearing sessions
+            session_unset();
             session_destroy();
             exit;
         }
@@ -53,9 +62,9 @@
     <h3>Change Password</h3>
     <form action="<?php echo $_SERVER['PHP_SELF']?>" method="post">
         <label for="OldPassword">Old Password: </label>
-        <input type="password" name="OldPassword"><br>
+        <input type="password" name="OldPassword" required><br>
         <label for="NewPassword">New Password: </label>
-        <input type="password" name="NewPassword"><br>
+        <input type="password" name="NewPassword" required><br>
         <button name="change" type="submit">Change Password</button>
     </form>
 </body>
