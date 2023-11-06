6fa3b64b122b07458b39d8a82bd68080aa17101b
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2023-11-06 11:42:52 +0400

    FormWithExistingData

--- TravelPlanner/Dashboard/EditProfile/index.php
+++ TravelPlanner/Dashboard/EditProfile/index.php
@@ -11,6 +11,38 @@
         echo '<a href="../../index.php">Home</a><br>';
         exit;
     }
+
+    // Execute Query
+    function QueryExecuteUpdate($column, $value){
+        require __DIR__."/../../var.php";
+
+        $QueryUpdate = "UPDATE user SET var1 = var2 WHERE email='".$_SESSION['email']."'";
+        $VarReference = array(&$column, &$value);
+        $updateVar = ["var1", "var2"];
+        $QueryStr = str_replace($updateVar, $VarReference, $QueryUpdate);
+        $result = $Mysqli->query($QueryStr);
+
+        return $result;
+    }
+
+    // Execute Query
+    function QueryExecuteSelect($column){
+        require __DIR__."/../../var.php";
+
+        $QuerySelect = "SELECT var1 FROM user WHERE email='".$_SESSION['email']."'";
+        $QueryStr = str_replace("var1", $column, $QuerySelect);
+        $result = $Mysqli->query($QueryStr);
+
+        return $result->fetch_row()[0];
+    }
+
+    // Loading Data From Database
+    $Picture = QueryExecuteSelect("image");
+    $Prefix = QueryExecuteSelect("prefix");
+    $Name = QueryExecuteSelect("name");
+    $Number = QueryExecuteSelect("number");
+    $Gender = QueryExecuteSelect("gender");
+    $DOB = QueryExecuteSelect("dob");
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -24,5 +56,41 @@
             display: none;
             }
     </style>
+    <body>
+        <?php include __DIR__."/../inc/header.php"?>
+        <h2>Edit Profile</h2>
+        <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post" enctype="multipart/form-data">
+            <label for="image">Picture: </label>
+            <input name="image" type="file">
+            <img src="<?php if($Picture){ echo ("data:image/jpeg;base64, ".base64_encode($Picture));}?>" width="40px" height="40px"/>
+            <br>
+            <label for="prefix">Prefix: </label>
+            <select name="prefix">
+                <option value="">--Select--</option>
+                <option value="Mr." <?php if($Prefix && $Prefix=="Mr.") echo "selected"; ?>>Mr.</option>
+                <option value="Ms." <?php if($Prefix && $Prefix=="Ms.") echo "selected"; ?>>Ms.</option>
+                <option value="Mrs." <?php if($Prefix && $Prefix=="Mrs.") echo "selected"; ?>>Mrs.</option>
+                <option value="Dr." <?php if($Prefix && $Prefix=="Dr.") echo "selected"; ?>>Dr.</option>
+                <option value="Adv." <?php if($Prefix && $Prefix=="Adv.") echo "selected"; ?>>Adv.</option>
+            </select><br>
+            <label for="name">Name: </label>
+            <input type="text" name="name" value="<?php echo $Name; ?>"><br>
+            <label for="email">Email: </label>
+            <input type="text" name="email" value="<?php echo $_SESSION['email']; ?>" disabled><br>
+            <label for="number">Number: </label>
+            <input name="number" type="tel" value="<?php echo $Number; ?>"><br>
+            Gender: 
+            <input type="radio" name="gender" value="m"  <?php if(isset($Gender) && $Gender=="m"){echo "checked";}; ?>>Male
+            <input type="radio" name="gender" value="f" <?php if(isset($Gender) && $Gender=="f"){echo "checked";}; ?>>Female
+            <input type="radio" name="gender" value="o" <?php if(isset($Gender) && $Gender=="o"){echo "checked";}; ?>>Other
+            <br>
+            <label for="dob">Date of Birth: </label>
+
+            <!-- Date Client Validation Min-100yrs max-tdy  -->
+            <input name="dob" type="date" value="<?php echo $DOB; ?>" min="<?php echo date("Y-m-d", time()-(86400*36500)) ?>" max="<?php echo date("Y-m-d") ?>"><br>
+            <button type="submit" name="profEdit">Save</button>
+        </form>
+        <br>
+        <a href="Password.php"><button type="button">Edit Password</button></a>
     </body>
 </html>
