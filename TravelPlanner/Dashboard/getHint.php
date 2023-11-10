98977cf5723ea0b33d95ada3857bb88a7068121b
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2023-11-10 15:29:38 +0400

    LogInValidation

--- TravelPlanner/Dashboard/getHint.php
+++ TravelPlanner/Dashboard/getHint.php
@@ -1,5 +1,15 @@
 <?php
 
+    // Session Start to Check if User Logged In
+    session_start();
+
+    // Checking If User is Logged In
+    if(!isset($_SESSION['email'])){
+        echo "<h3>You Are Logged out Please Login Again!!!</h3><br>";
+        echo '<a href="../index.php">Home</a><br>';
+        exit;
+    }
+
 // Cities Local Database with distance and Hotels with Rate
 $city = [
     // Template: 'CityName=>[0=>distance, 1=>['HotelName'=>RateInAED, ...] ]'
