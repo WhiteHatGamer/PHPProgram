5f5d0a759ebedb83d7b18151868188e89b46128e
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2023-11-10 14:33:48 +0400

    UpdatingSQL

--- TravelPlanner/Dashboard/ListStays/index.php
+++ TravelPlanner/Dashboard/ListStays/index.php
@@ -13,14 +13,20 @@
         exit;
     }
 
-    // Debugging Print
-    // print_r($_POST);
-    // echo "<br>";
-
+    // Edit Form With Autocomplete
     if(isset($_POST['edit'])){
         echo "<br>Edit Details: <br>";
-        $result = $Mysqli->query("SELECT * FROM $StayTable WHERE id={$_POST['edit']}");
-        echo "<form method='post' name='stay' action={$_SERVER['PHP_SELF']}'><table border='1'>
+
+        // Getting Results Based on id
+        $result = $Mysqli->query("SELECT * FROM $StayTable WHERE id={$_POST['edit']}")->fetch_all()[0];
+        $city = $result[1];
+        $Hotel = $result[2];
+        $checkIn = strtotime($result[3]);
+        $checkOut = strtotime($result[4]);
+        $night = round(($checkOut-$checkIn)/60/60/24);
+
+        // Form Table start
+        echo "<form method='post' name='stay' action='{$_SERVER['PHP_SELF']}'><table border='1'>
             <tr>
                 <th>City</th>
                 <th>Hotel</th>
@@ -30,7 +36,7 @@
             </tr>";
         echo "<tr>";
         ?>
-            <td><input type="text" name="city" id="city" placeholder="Type City" required list="id_list" onkeyup="getCity(this.value)" onblur="getHotels(this.value)" autocomplete="off"></td>
+            <td><input type="text" name="city" id="city" placeholder="Type City" required list="id_list" onkeyup="getCity(this.value)" onblur="getHotels(this.value)" autocomplete="off" value="<?=$city?>" autofocus></td>
             <datalist id="id_list">
                 <div id="id_city">
                     <!-- To be filled onKeyUp type -->
@@ -39,35 +45,66 @@
             </datalist>
 
             <!-- Select List Based on Selected City -->
-            <td><select id="id_hotel" name="id_hotel" onchange=getExpense(this.value)>
-                <option value="">--Select-Hotel--</option>
+            <td><select id="id_hotel" name="id_hotel" onchange=getExpense(this.value) required>
+                <option value="<?=$Hotel?>"><?=$Hotel?></option>
             </select></td>
-            <output name="HotelExpense" id="HotelExpense"></output>
             <br>
             <br>
             
-            <!-- <label for="checkIn">Check In: </label>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
-            <label for="checkOut">Check Out: </label> -->
             <!-- Date with Limit using PHP -->
-            <td><input style="width: 130px;" type="date" name="checkIn" id="checkIn" required min="<?php echo date('Y-m-d');?>"></td>
-            <td><input style="width: 130px;" type="date" name="checkOut" id="checkOut" onchange="calculateExpense(this.value)" required min="<?php echo date('Y-m-d');?>"></td>
-            <td><select id="night" name="night" onchange="calculateDate(this.value)">
+            <td>
+                <input style="width: 130px;" type="date" name="checkIn" id="checkIn" required
+                    min="<?php echo date('Y-m-d');?>" value="<?=date('Y-m-d',$checkIn)?>">
+            </td>
+            <td>
+                <input  style="width: 130px;" type="date" name="checkOut" id="checkOut" onchange="calculateExpense(this.value)"
+                    min="<?php echo date('Y-m-d');?>" value="<?=date('Y-m-d',$checkOut)?>" max="checkIn.value" required>
+            </td>
+            <td>
+                <select id="night" name="night" onchange="calculateDate(this.value)" required>
                 <?php for ($i=1; $i < 31; $i++) { 
-                    echo "<option value='$i'>$i Night(s)</option>";
+                    echo "<option value='$i' ".(($i==$night)? 'selected':'').">$i Night(s)</option>";
                 } ?>
-                <option value="31">30+ Night(s)</option>
-            </select></td>
-            <td><button type="submit" name="stay">&#10003;</button></form></td>
+                <option value="31" <?=($night>30)?'selected':''?>>30+ Night(s)</option>
+                </select>
+            </td>
+            <td>
+                <button type="submit" name="stay" value="<?=$_POST['edit']?>">&#10003;</button></form>
+            </td>
             <form method="post">
-                <td><button type="submit" name="cancel">&#9747;</button></td>
+                <td>
+                    <button type="submit" name="cancel">&#9747;</button>
+                </td>
             </form>
-            </table></form>
+        </table>
+        <output name="HotelExpense" id="HotelExpense">
+            <!-- Filling Estimate on change -->
+        </output>
         <?php
+    }
+
+    if(isset($_POST['stay'])){
+
+        // Edit Form Updating with Exception Handling
+        try {
+            $Mysqli->query(
+                "UPDATE $StayTable
+                SET city='{$_POST['city']}',
+                    hotel = '{$_POST['id_hotel']}',
+                    checkIn = '{$_POST['checkIn']}',
+                    checkOut = '{$_POST['checkOut']}'
+                WHERE email='{$_SESSION['email']}' && id={$_POST['stay']}");
+            echo "Details Updated <br>";
+        } catch (\Throwable $th) {
+            echo "Some Error Occurred: {$th->getMessage()}";
+        }
     }
 
     // Delete Confirmation
     if(isset($_POST['delete'])){
         echo "<br>Confirm <b>Delete</b>: <br>";
+
+        // Listing Selected Row to confirm
         $result = $Mysqli->query("SELECT * FROM $StayTable WHERE id={$_POST['delete']}");
         echo "<form method='post' action='{$_SERVER['PHP_SELF']}'><table border='1'>
             <tr>
@@ -81,7 +118,11 @@
             if(($key=="id") || ($key=='email')){
                 continue;
             }
-            echo "<td>".$value."</td>";
+            if($key=='checkIn' || $key=='checkOut'){
+                echo "<td>".date('d-M-Y',strtotime($value))."</td>";
+            }else{
+                echo "<td>".$value."</td>";
+            }
         }
 
         // Button with id as Value
@@ -111,9 +152,11 @@
 
     try{
         $result = $Mysqli->query("SELECT * FROM $StayTable WHERE(email='{$_SESSION['email']}') ORDER BY checkIn");
+
         if(!$result->num_rows){
+
             // No Data Stored
-            echo "<br>Sorry!! No Trip is in Database <br>Please Add journey Using This <b><a href='../PlanTrip/index.php'>Link</a></b>";
+            echo "<br>Sorry!! No Hotel Check-In is in Database <br>Please Add Your Stay Using This <b><a href='../AppointHotel/index.php'>Link</a></b>";
         }else{
             
             // Details Available
@@ -126,6 +169,8 @@
                     <th>CheckOut</th>
                     <th>Edit</th>
                 </tr>";
+
+            // Listing Table Data
             for($i=0;$i<$result->num_rows;$i++){
                 echo "<tr>";
                 foreach ($result->fetch_assoc() as $key => $value) {
@@ -135,7 +180,11 @@
                     }elseif($key=='email'){
                         continue;
                     }
-                    echo "<td>".$value."</td>";
+                    if($key=='checkIn' || $key=='checkOut'){
+                        echo "<td>".date('d-M-Y',strtotime($value))."</td>";
+                    }else{
+                        echo "<td>".$value."</td>";
+                    }
                 }
                 echo "<td><button type='submit' name='edit' value=$id>&#9874;</button>";
                 echo "<button type='submit' name='delete' value=$id>&#9760;</button></td>";
