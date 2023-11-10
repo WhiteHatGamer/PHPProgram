d34ef4c7f0daf9a4ec83288d4e48ae1fb3ecfd5b
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2023-11-10 15:29:38 +0400

    LogInValidation

--- TravelPlanner/Dashboard/CreateNote/index.php
+++ TravelPlanner/Dashboard/CreateNote/index.php
@@ -1,6 +1,13 @@
 <?php
     // Starting Session
     session_start();
+
+    // Checking If User is Logged In
+    if(!isset($_SESSION['email'])){
+        echo "<h3>You Are Logged out Please Login Again!!!</h3><br>";
+        echo '<a href="../../index.php">Home</a><br>';
+        exit;
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
