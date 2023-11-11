7d18167b7c79e21b60e45a90a41a69c00af0ee5c
WhiteHatGamer <thahirmoolakkadath@gmail.com>
2023-11-11 11:23:02 +0400

    SQLTemplateCreateTableJourney

--- TravelPlanner/sql/SetupDatabase.sql
+++ TravelPlanner/sql/SetupDatabase.sql
@@ -4,3 +4,17 @@
 -- Create Database
 CREATE DATABASE IF NOT EXISTS `form`;
 USE `form`;
+
+-- Create User Table
+CREATE TABLE user(
+    `email` VARCHAR(255) NOT NULL,
+    `password` VARCHAR(260) NOT NULL,
+    `name` VARCHAR(100),
+    `number` INT,
+    `dob` DATE,
+    `image` BLOB,
+    `gender` enum('m','f','o'),
+    `prefix` enum('Mr.','Ms.','Mrs.','Dr.','Adv.'),
+    PRIMARY KEY(`email`)
+)
+
